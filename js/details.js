77f62413bd75f7be020f49fcb352220456105c3b
Anthony <2060246@cstj.qc.ca>
2022-10-24 13:42:41 -0400

    Changed generate monster diplay monster at top

--- js/details.js
+++ js/details.js
@@ -2,6 +2,7 @@ const ELEMENT_IMG_URL = 'https://assets.andromia.science/elements';
 const SERVICE_URL = `https://api.andromia.science/monsters/atlas/`;
 const GENERATE_URL = "https://api.andromia.science/monsters/"
 const urlParams = {};
+const char = String.fromCharCode(160);
 const regex = new RegExp('([A-Z]{1,2})([1-9]{1,3})'); //regex pour faire la validation de la position 1 à 2
 (window.onpopstate = function () {
   let match;
@@ -128,13 +129,13 @@ async function genererMonstre(atlasNumber) {
       tabHash = string.match(/.{6}/g)
 
       tabHash.forEach(h => {
-        monstersDts += `<span class="block" style="color:#${h}; background-color:#${h}">${h}</span>`
+        monstersDts += `<span class="block" style="color:#${h}; background-color:#${h}">${char}${char}</span>`
       });
 
       monstersDts += `${monsterDetails.hash.substring(62)}`
       monstersDts += '</div></td></tr>';
 
-      $('#monsterTab tbody').append(monstersDts);
+      $('#monsterTab tbody').prepend(monstersDts);
 
     }
   } catch (err) {
@@ -176,7 +177,7 @@ async function retrieveDetailsMonsters(atlasNumber) {
         tabHash = string.match(/.{6}/g)
 
         tabHash.forEach(h => {
-          monstersDts += `<span class="block" style="color:#${h}; background-color:#${h}">${h}</span>`
+          monstersDts += `<span class="block" style="color:#${h}; background-color:#${h}">${char}${char}</span>`
         });
 
         monstersDts += `${s.hash.substring(62)}`
@@ -196,87 +197,3 @@ async function retrieveDetailsMonsters(atlasNumber) {
 
   }
 }
-
-
-
-
-
-// async function retrievePlanet(href) {
-//   try {
-//     const response = await axios.get(href);
-//     if (response.status === 200) {
-//       const planet = response.data;
-//       $('#lblName').html(planet.name);
-//       $('#lblDiscoveredBy').html(planet.discoveredBy);
-//       $('#lblDiscoveryDate').html(planet.discoveryDate);
-//       $('#lblTemperature').html(planet.temperature);
-//       $('#lblPosition').html(`(${planet.position.x.toFixed(3)} ; ${planet.position.y.toFixed(3)} ; ${planet.position.z.toFixed(3)})`);
-
-//       $('#imgIcon').attr('src', planet.icon);
-
-//       //Afficher les satellites
-
-//       if (planet.satellites.length > 0) {
-//         planet.satellites.forEach(s => {
-//           $('#satellites').append(`<li>${s}</li>`);
-//         })
-//       } else {
-//         $('#satellites').append('Aucun satellite');
-//       }
-
-//       //Afficher les portals
-//       displayPortals(planet.portals);
-
-
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-
-
-// function displayPortals(portals) {
-
-//   portals.forEach(p => {
-
-//     //Créer le html (tr)
-//     const infoPortal = displayPortal(p);
-
-//     //Injecter le html
-//     $('#portals tbody').append(infoPortal);
-
-//   });
-
-// }
-
-// function displayPortal(portal) {
-//   let infoPortal = '<tr>';
-//   infoPortal += `<td>${portal.position}</td>`;
-//   infoPortal += `<td><img class="affinity" src="img/${portal.affinity}.svg" /></td>`;
-//   infoPortal += '</tr>';
-//   return infoPortal;
-// }
-
-
-// async function addPortal() {
-//   const body = {
-//     position: $('#txtPortalPosition').val(),
-//     affinity: $('#cboAffinity').val()
-//   };
-
-//   const URL = `${urlParams.planet}/portals`;
-
-//   try {
-//     const response = await axios.post(URL, body);
-//     if (response.status === 201) {
-//       const infoPortal = displayPortal(response.data);
-
-//       $('#portals tbody').prepend(infoPortal);
-
-//     }
-//   } catch (err) {
-//     console.log(err);
-//   }
-
-// }
